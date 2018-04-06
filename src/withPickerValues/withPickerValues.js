5cbf4294a4c10a3f657113342f2ff754ac3e5ec4
Thomas Pucci <thomas.lb.pucci@gmail.com>
2018-04-06 15:51:10 +0200

    fix(package): Fix close on picker value change and let use picker with async values

--- src/withPickerValues/withPickerValues.js
+++ src/withPickerValues/withPickerValues.js
@@ -3,13 +3,22 @@
 import React from 'react';
 import PickerModal from './PickerModal';
 
-const withPickerModal = Component => props => {
-  const selectedItem = props.values.find(item => item.value === props.value);
-  return (
-    <PickerModal {...props}>
-      <Component {...props} value={selectedItem ? selectedItem.label : ''} />
-    </PickerModal>
-  );
+const withPickerModal = Component => {
+  class WithPickerModal extends React.Component<$FlowFixMeProps, $FlowFixMeState> {
+    render() {
+      const selectedItem =
+        this.props.values && this.props.values.length > 0
+          ? this.props.values.find(item => item && item.value === this.props.value)
+          : undefined;
+      return (
+        <PickerModal {...this.props}>
+          <Component {...this.props} value={selectedItem ? selectedItem.label : ''} />
+        </PickerModal>
+      );
+    }
+  }
+
+  return WithPickerModal;
 };
 
 export default withPickerModal;
