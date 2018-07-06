fe129b5a7087b666729fd892a72c5b7e1e5f7742
Jérémy Gotteland <jeremy.gotteland@gmail.com>
2018-07-06 10:23:26 +0100

    refactor: Replace custom function with lodash get

--- src/makeReactNativeField.js
+++ src/makeReactNativeField.js
@@ -1,11 +1,12 @@
 import { compose, mapProps } from "recompose";
+import _ from 'lodash';
+
 import withFormik from "./withFormik";
-import { selectValue } from './utils';
 
 const makeReactNativeField = compose(
   withFormik,
   mapProps(({ formik: { setFieldValue, setFieldTouched, values }, name, ...props }) => ({
-    value: selectValue(values, name),
+    value: _.get(values, name),
     ...props,
     name,
     onChangeText: text => {
--- src/withError.js
+++ src/withError.js
@@ -1,11 +1,12 @@
 import { compose, mapProps } from "recompose";
+import _ from 'lodash';
+
 import withFormik from "./withFormik";
-import { selectValue } from './utils';
 
 const withError = compose(
   withFormik,
   mapProps(({ formik: { errors }, name, ...props }) => ({
-    error: selectValue(errors, name),
+    error: _.get(errors, name),
     ...props,
     name
   }))
