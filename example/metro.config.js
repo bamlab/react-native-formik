e9bd71af302d63c85d42bc713f139c416ccc376d
AntoineDoubovetzky <antoined@bam.tech>
2020-03-21 17:02:12 +0100

    chore(example): add example folder

---
+++ example/metro.config.js
@@ -0,0 +1,26 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+const path = require('path');
+
+module.exports = {
+  projectRoot: path.resolve(__dirname),
+  watchFolders: [path.resolve(__dirname, '../dist')],
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+  resolver: {
+    extraNodeModules: {
+      'react-native-formik': path.resolve(__dirname, '../dist'),
+    },
+  },
+};
